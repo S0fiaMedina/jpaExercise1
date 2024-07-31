272d14049e0f331c5c5f4b5a66f5721cbb98f4d7
S0fiaMedina <151967289+S0fiaMedina@users.noreply.github.com>
2024-07-31 18:18:49 -0500

    feat: :sparkles: adds error handling for responses and surveys

--- src/main/java/com/jpaexercise/jpaexercise/web/controller/ResponseOptionController.java
+++ src/main/java/com/jpaexercise/jpaexercise/web/controller/ResponseOptionController.java
@@ -1,11 +1,14 @@
 package com.jpaexercise.jpaexercise.web.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.jpaexercise.jpaexercise.domain.services.responseOption.IResponseOption;
 import com.jpaexercise.jpaexercise.persistence.entities.ResponseOption;
+
+import jakarta.validation.Valid;
 
 
 
@@ -46,13 +51,27 @@ public class ResponseOptionController {
 
     // post an object
     @PostMapping
-    public ResponseEntity<ResponseOption> create(@RequestBody ResponseOption responseOption){
+    public ResponseEntity<?> create( @Valid @RequestBody ResponseOption responseOption, 
+            BindingResult result) {
+            
+        // revisa por errores
+        if (result.hasFieldErrors()) {
+            return validation(result);
+        }
+
         ResponseOption responseOptionNew = this.service.save(responseOption);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseOptionNew);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ResponseOption> update(@PathVariable Long id, @RequestBody ResponseOption responseOption){
+    public ResponseEntity<?> update(@PathVariable Long id, @Valid @RequestBody ResponseOption responseOption, 
+            BindingResult result){
+
+        // revisa por errores
+        if (result.hasFieldErrors()) {
+            return validation(result);
+        }   
+
         Optional<ResponseOption> responseOptionOptional = this.service.update(id, responseOption);
         if (responseOptionOptional.isPresent()){
             return ResponseEntity.status(HttpStatus.CREATED).body(responseOptionOptional.orElseThrow());
@@ -74,5 +93,15 @@ public class ResponseOptionController {
 
         return ResponseEntity.notFound().build();
 
+    }
+
+        // metodo de validacion
+    private ResponseEntity<Map<String, String>> validation(BindingResult result) {
+        Map<String, String> errors = new HashMap<>();
+
+        result.getFieldErrors().forEach(err -> {
+            errors.put(err.getField(), "El campo " + err.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(errors);
     }
 }
--- src/main/java/com/jpaexercise/jpaexercise/web/controller/SurveyController.java
+++ src/main/java/com/jpaexercise/jpaexercise/web/controller/SurveyController.java
@@ -1,11 +1,14 @@
 package com.jpaexercise.jpaexercise.web.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.jpaexercise.jpaexercise.domain.services.survey.ISurvey;
 import com.jpaexercise.jpaexercise.persistence.entities.Survey;
+
+import jakarta.validation.Valid;
 
 
 
@@ -46,13 +51,27 @@ public class SurveyController {
 
     // post an object
     @PostMapping
-    public ResponseEntity<Survey> create(@RequestBody Survey survey){
+    public ResponseEntity<?> create(@Valid @RequestBody Survey survey, BindingResult result){
+        
+
+        // revisa por errores
+        if (result.hasFieldErrors()) {
+            return validation(result);
+        }   
+        
         Survey surveyNew = this.service.save(survey);
         return ResponseEntity.status(HttpStatus.CREATED).body(surveyNew);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Survey> update(@PathVariable Long id, @RequestBody Survey survey){
+    public ResponseEntity<?> update(@PathVariable Long id, @Valid @RequestBody Survey survey,
+        BindingResult result){
+
+        // revisa por errores
+        if (result.hasFieldErrors()) {
+            return validation(result);
+        }   
+
         Optional<Survey> surveyOptional = this.service.update(id, survey);
         if (surveyOptional.isPresent()){
             return ResponseEntity.status(HttpStatus.CREATED).body(surveyOptional.orElseThrow());
@@ -65,7 +84,6 @@ public class SurveyController {
     public ResponseEntity<Survey> delete(@PathVariable Long id){
         Survey survey = new Survey();
         survey.setId(id);
-
         Optional<Survey> optionalSurvey = this.service.delete(id);
 
         if (optionalSurvey.isPresent()){
@@ -74,5 +92,14 @@ public class SurveyController {
 
         return ResponseEntity.notFound().build();
 
+    }
+        // metodo de validacion
+    private ResponseEntity<Map<String, String>> validation(BindingResult result) {
+        Map<String, String> errors = new HashMap<>();
+
+        result.getFieldErrors().forEach(err -> {
+            errors.put(err.getField(), "El campo " + err.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(errors);
     }
 }
